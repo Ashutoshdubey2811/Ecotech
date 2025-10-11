005c32886a4506eac27b7210c40a4c9bc5e97fa3
Ashutoshdubey2811 <98604834+Ashutoshdubey2811@users.noreply.github.com>
2025-10-11 21:12:43 +0530

    Model training

--- app.py
+++ app.py
@@ -1,22 +1,30 @@
-from src.Ecotech_project.logger import logging 
+from src.Ecotech_project.logger import logging
 from src.Ecotech_project.exception import CustomException
 from src.Ecotech_project.components.data_ingestion import DataIngestion
 from src.Ecotech_project.components.data_ingestion import DataIngestionConfig
-from src.Ecotech_project.components.data_transformation import DataTransformationConfig, DataTransformation
+from src.Ecotech_project.components.data_transformation import DataTransformationConfig,DataTransformation
+from src.Ecotech_project.components.model_trainer import ModelTrainerConfig,ModelTrainer
 
 import sys
 
-if __name__ == "__main__":
-    logging.info("Starting the application...")
+
+if __name__=="__main__":
+    logging.info("The execution has started")
 
     try:
-        #data_ingestion_config = DataIngestionConfig()
-        data__ingestion = DataIngestion()
-        train_data_path,test_data_path=data__ingestion.initiate_data_ingestion()
+        #data_ingestion_config=DataIngestionConfig()
+        data_ingestion=DataIngestion()
+        train_data_path,test_data_path=data_ingestion.initiate_data_ingestion()
 
-        data_transformation_config= DataTransformationConfig()
-        data_transformation = DataTransformation()
-        data_transformation.initiate_data_transformation(train_data_path,test_data_path)
+        #data_transformation_config=DataTransformationConfig()
+        data_transformation=DataTransformation()
+        train_arr,test_arr,_=data_transformation.initiate_data_transormation(train_data_path,test_data_path)
+
+        ## Model Training
+
+        model_trainer=ModelTrainer()
+        print(model_trainer.initiate_model_trainer(train_arr,test_arr))
+        
     except Exception as e:
-        logging.info("An exception occurred")
-        raise CustomException(e, sys)
+        logging.info("Custom Exception")
+        raise CustomException(e,sys)
--- src/Ecotech_project/exception.py
+++ src/Ecotech_project/exception.py
@@ -1,27 +1,23 @@
 import sys
 from src.Ecotech_project.logger import logging
 
-class CustomException(Exception):
-    def __init__(self, error_message, error_detail: sys):
-        super().__init__(error_message)
-        self.error_message = CustomException.error_message_detail(error_message, error_detail)
+def error_message_detail(error,error_detail:sys):
+    _, _, exc_tb = sys.exc_info()
 
-    @staticmethod
-    def error_message_detail(error_message, error_detail: sys) -> str:
-        """Return a detailed error message with file name and line number."""
-        _, _, exc_tb = error_detail.exc_info()
-        line_number = exc_tb.tb_lineno
-        file_name = exc_tb.tb_frame.f_code.co_filename
-        line_number = exc_tb.tb_lineno
-        detailed_message = (
-            f"Error occurred in script: {file_name} "
-            f"at line number: {line_number} "
-            f"with message: {error_message}"
-        )
-        return detailed_message
+    file_name=exc_tb.tb_frame.f_code.co_filename
+    error_message="Error occured in python script name [{0}] line number [{1}] error message[{2}]".format(
+     file_name,exc_tb.tb_lineno,str(error))
+
+    return error_message
+
+
+class CustomException(Exception):
+    def __init__(self,error_message,error_details:sys):
+        super().__init__(error_message)
+        self.error_message=error_message_detail(error_message,error_details)
 
     def __str__(self):
         return self.error_message
 
     def __repr__(self):
-        return f"{self.__class__.__name__}({self.error_message})"
+        return f"{self.__class__.__name__}({self.error_message}) "
